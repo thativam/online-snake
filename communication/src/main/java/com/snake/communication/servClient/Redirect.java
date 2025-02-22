53d28a0aefda9ecb1106b16430976a64118063fe
Ivan_cn <ivan.capeli@catijr.com.br>
2025-02-22 11:50:44 -0300

    feat: sm-client done, should start client-sm

--- communication/src/main/java/com/snake/communication/servClient/Redirect.java
+++ communication/src/main/java/com/snake/communication/servClient/Redirect.java
@@ -3,4 +3,8 @@ package com.snake.communication.servClient;
 public class Redirect {
     int port;
     String addr;
+    public Redirect(int port, String addr) {
+        this.port = port;
+        this.addr = addr;
+    }
 }
