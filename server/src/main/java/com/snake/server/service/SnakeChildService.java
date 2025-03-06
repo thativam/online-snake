f5ac65be1eed0cc1b04a7799ac53731461f17c86
Ivan_cn <ivan.capeli@catijr.com.br>
2025-03-05 21:09:05 -0300

    feat: server with clients connected

--- server/src/main/java/com/snake/server/service/SnakeChildService.java
+++ server/src/main/java/com/snake/server/service/SnakeChildService.java
@@ -1,13 +1,23 @@
 package com.snake.server.service;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import com.esotericsoftware.kryonet.Connection;
 import com.snake.server.domain.ISnakeChildServer;
 import com.snake.server.domain.SnakeChildServer;
 import com.snake.server.utils.ChildServerUtils;
 
 public class SnakeChildService implements ISnakeChildService {
     private ISnakeChildServer server;
+    private List<ISnakeChildServer> activeServers = new ArrayList<>();
+
+    public List<ISnakeChildServer> getActiveServers() {
+        return activeServers;
+    }
 
     public int startChild() {
         server = new SnakeChildServer();
@@ -20,6 +30,7 @@ public class SnakeChildService implements ISnakeChildService {
             server.bind(port);
             server.addClientListener();
             server.start();
+            activeServers.add(server); // TODO how to control this list? i.e when to remove server
             System.out.println("[SERVER] Child server started on port " + port);
             System.out.println("[SERVER] Wainting for client to connect...");
         } catch (IOException e) {
@@ -29,6 +40,15 @@ public class SnakeChildService implements ISnakeChildService {
     }
 
     public void stopChild() {
+    }
+
+    public Map<Integer, Connection[]> getActiveConnections() {
+        HashMap<Integer, Connection[]> activeConnections = new HashMap<>();
+        for (ISnakeChildServer server : activeServers) {
+            activeConnections.put(server.getPort(), server.getActiveConnections());
+            server.getActiveConnections();
+        }
+        return activeConnections;
     }
 
 }
