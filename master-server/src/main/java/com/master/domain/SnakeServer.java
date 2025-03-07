9ff6908f6a200217c6cccdf6e8ae590def68243e
thativam <thativancapeli@outlook.com>
2025-03-07 11:16:48 -0300

    feat: connecting 4 clients to the server, just create new servers when one is full.

--- master-server/src/main/java/com/master/domain/SnakeServer.java
+++ master-server/src/main/java/com/master/domain/SnakeServer.java
@@ -1,9 +1,13 @@
 package com.master.domain;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.SortedMap;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
 
 import com.esotericsoftware.kryonet.Connection;
 import com.esotericsoftware.kryonet.Listener;
@@ -15,8 +19,9 @@ import com.snake.server.service.SnakeChildService;
 public class SnakeServer implements ISnakeServer {
     private Server server;
     private ISnakeChildService childService;
-    private List<Connection> connections = Collections.synchronizedList(new ArrayList<>());
-
+    // TODO: how to control this list? i.e when to remove server
+    private Set<Integer> connections = Collections.synchronizedSet( new HashSet<>()); // store just the port, but should save host+port for each generated server.
+    private static final int MAX_PLAYERS = 4;
     public SnakeServer() {
         this.server = new Server();
         this.childService = new SnakeChildService();
@@ -39,11 +44,13 @@ public class SnakeServer implements ISnakeServer {
             @Override
             public void connected(Connection connection) {
                 System.out.println("Client connected to master from " + connection.getRemoteAddressTCP());
-                connections.add(connection);
+                
                 try {
+                    // logic to create/use a child server
                     // Create and start a child server on a dynamic port
-                    int port = childService.startChild();
+                    int port = handleNewServers() == -1 ? childService.startChild() : handleNewServers();
                     System.out.println("Spun up child server on port " + port);
+                    connections.add(port);
 
                     // Prepare the redirect message with the new port number
                     Redirect redirect = new Redirect(port, "localhost");
@@ -65,8 +72,27 @@ public class SnakeServer implements ISnakeServer {
             @Override
             public void disconnected(Connection connection) {
                 System.out.println("Client disconnected from master");
-                connections.remove(connection);
             }
         });
+    }
+
+    private int handleNewServers(){
+        Map<Integer, Connection[]> activeConnections = childService.getActiveConnections();
+        if(activeConnections.isEmpty()) {
+            System.out.println("No active connections");
+            return -1;
+        }
+
+        @SuppressWarnings("unused")
+        SortedMap<Integer, Connection[]> sortedConnections = activeConnections.entrySet().stream()
+            .sorted((entry1, entry2) -> Integer.compare(entry1.getValue().length, entry2.getValue().length))
+            .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1,e2) -> e1, TreeMap::new));
+        
+        while ((sortedConnections.size() > 0) && (sortedConnections.get(sortedConnections.lastKey()).length == MAX_PLAYERS)) {
+            sortedConnections.remove(sortedConnections.lastKey());
+        }
+
+        return sortedConnections.isEmpty() ? -1 : sortedConnections.lastKey(); // with the most connection
+        // use strategy pattern ?
     }
 }
