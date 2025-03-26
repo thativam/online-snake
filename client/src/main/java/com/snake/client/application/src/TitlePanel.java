71e16f3e4866972e72b070007aa692e820735927
Ivan_cn <ivan.capeli@catijr.com.br>
2025-03-26 19:38:07 -0300

    refactor: title panel extracted, should use on snake game since is the main frame.

---
+++ client/src/main/java/com/snake/client/application/src/TitlePanel.java
@@ -0,0 +1,24 @@
+package com.snake.client.application.src;
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+
+public class TitlePanel extends JPanel {
+    private static final long serialVersionUID = 1L;
+    private final ImageIcon titleImage;
+
+    public TitlePanel(String imageBasePath) {
+        this.titleImage = new ImageIcon(imageBasePath + "title.png");
+        setPreferredSize(new Dimension(900, 70));
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawRect(24, 10, 852, 55);
+        titleImage.paintIcon(this, g, 25, 11);
+    }
+}
